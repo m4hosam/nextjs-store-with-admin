a58040d6a9af30d47ae7c8e0fd027166669b0b9e
MOHAMED HOSAM <mohamedhosaam154@gmail.com>
2023-07-09 21:49:22 +0300

    creating admin section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+// import prismadb from '@/lib/prismadb'
+import { Navbar } from '@/components/Navbar'
+import { Card } from '@/components/Card'
+import { SearchBar } from '@/components/ui/searchBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +18,26 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <Navbar />
+        <SearchBar />
+        <div className='flex w-full flex-row flex-wrap justify-center'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ components/Card.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Card = () => {
+    return (
+        <div className="flex flex-col justify-between items-center
+        w-72 max-w-sm bg-white border
+        border-gray-200 rounded-lg shadow
+        dark:bg-gray-800 dark:border-gray-700 m-7"
+        >
+            <a href="#">
+                <img className="rounded-t-lg px-6 pt-6"
+                    src="https://media.gq-magazine.co.uk/photos/6409fb2d0a046f0156dd535d/4:3/w_1440,h_1080,c_limit/Best-gold-watches-HP.jpg" alt="product image" />
+            </a>
+            <a href="#">
+                <h5 className="text-xl text-center p-6 font-semibold tracking-tight text-gray-900 dark:text-white">
+                    Apple Watch Series 7 GPS, Aluminium Case
+                </h5>
+            </a>
+            <div className="flex px-6 mb-6 items-center justify-between">
+                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                    599 EGP
+                </span>
+            </div>
+            <a href="#" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-b-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Add to cart
+            </a>
+
+        </div>
+    )
+}
+
