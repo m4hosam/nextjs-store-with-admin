2503223aefc5efd50253523c03a616e97f297feb
MOHAMED HOSAM <mohamedhosaam154@gmail.com>
2023-07-25 16:44:53 +0200

    home route reads api and generates cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,50 @@
-import Image from 'next/image'
-import { Card } from '@/components/Card'
-import { SearchBar } from '@/components/ui/searchBar'
+"use client"
+import { useEffect, useState } from "react";
+import { Card } from "@/components/Card";
+import { SearchBar } from "@/components/ui/searchBar";
+import axios from "axios";
 
+type CardProps = {
+    name: string;
+    description: string;
+    price: string;
+    image: string;
+};
 
 export default function Home() {
+    const [products, setProducts] = useState<CardProps[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get('/api/products/read').then((response) => {
+            console.log(response.data);
+            setProducts(response.data);
+            setLoading(false);
+        });
+    }, []);
+
     return (
-        <main >
+        <main>
             <section className='mt-16'>
                 <h1 className='text-3xl w-full text-center font-bold'>Products</h1>
                 <SearchBar />
                 <div className='flex w-full flex-row flex-wrap justify-center px-7'>
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    {loading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        products.map((product, index) => (
+                            <Card
+                                key={index}
+                                name={product.name}
+                                description={product.description}
+                                price={product.price}
+                                image={product.image}
+                            />
+                        ))
+                    )}
                 </div>
             </section>
         </main>
-    )
+    );
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
-import watch from '@/public/assets/watch.webp'
+// import watch from '@/public/assets/watch.webp'
+
+type CardProps = {
+    name: string;
+    description: string;
+    price: string;
+    image: string;
+};
 
 
-export const Card = () => {
+export const Card = ({ name, description, price, image }: CardProps) => {
     return (
         <div className="flex flex-col justify-between items-center
         w-60 max-w-sm bg-white border
@@ -12,17 +19,19 @@ export const Card = () => {
         >
             <a href="#">
                 <Image className="rounded-t-lg px-6 pt-6"
-                    src={watch}
+                    src={image}
+                    width={500}
+                    height={500}
                     alt="product image" />
             </a>
             <a href="#">
                 <h5 className="text-xl text-center p-6 font-semibold tracking-tight text-gray-900 dark:text-white">
-                    Apple Watch Series 7 GPS, Aluminium Case
+                    {name}
                 </h5>
             </a>
             <div className="flex px-6 mb-6 items-center justify-between">
                 <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                    599 EGP
+                    {price} EGP
                 </span>
             </div>
             <a href="#" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-b-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
