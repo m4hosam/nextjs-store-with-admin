65359c0f26788f96a89ec0c04074c0dc1454539b
MOHAMED HOSAM <mohamedhosaam154@gmail.com>
2023-08-17 17:50:17 +0200

    Profile Page

--- app/(root)/account/page.tsx
+++ app/(root)/account/page.tsx
@@ -7,32 +7,46 @@ import { CartSchema } from "@/common.types";
 import { useShoppingCart } from "@/context/ShoppingCartContext"
 import AuthProviders from '@/components/AuthProviders'
 import { signIn, signOut, useSession } from 'next-auth/react'
-import { SessionInterface } from "@/common.types";
+
 import { ProfileAvatar } from '@/components/profileAvatar'
 // import { getCurrentUser } from "@/lib/session";
 import { getCsrfToken } from "next-auth/react"
 import { useRouter } from "next/navigation";
-import { CredentialsForm } from '@/components/loginForm'
 import { authOptions } from "@/lib/session";
 import { getServerSession } from "next-auth/next"
 import { redirect } from "next/navigation"
+import { Input } from "@/components/ui/input";
+import { InputForm } from "@/components/profileForm";
 
 
 
 export default async function Account() {
-    const session = await getServerSession(authOptions)
 
-    if (session) {
-        redirect('/')
-    }
 
     return (
-        <div
+        <div className="flex flex-col justify-between w-full sm:flex-row  sm:m-20">
+            <div className="flex flex-row items-center  justify-between sm:justify-start border-solid border-2 border-slate-300 sm:flex-col">
+                <Link href="/account"
+                    className="px-4 sm:px-16 py-4 text-sm font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 hover:bg-slate-200 w-full text-center"
+                >
+                    Account
+                </Link>
+                <Link href="/account"
+                    className="px-4 sm:px-16 py-4 text-sm font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 hover:bg-slate-200 w-full text-center"
+                >
+                    Addresses
+                </Link>
+                <Link href="/account"
+                    className="px-4 sm:px-16 py-4 text-sm font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 hover:bg-slate-200 w-full text-center"
+                >
+                    Orders
+                </Link>
 
-            className="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8"
-        >
 
-            <p>Account Details</p>
+            </div>
+            <div className="flex flex-col items-start justify-start w-4/6 py-7 sm:mx-1 mx-16">
+                <InputForm />
+            </div>
         </div>
     )
 }
---
+++ components/profileForm.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import toast, { Toaster } from 'react-hot-toast';
+
+const FormSchema = z.object({
+    name: z.string().min(2, {
+        message: "name must be at least 2 characters.",
+    }),
+    phone: z.string().min(10, {
+        message: "phone must be at least 10 characters.",
+    }),
+})
+
+export function InputForm() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            name: "Tom",
+            // Add default values for other fields if needed
+        },
+    })
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        console.log(data)
+        toast.success('Information has been updated')
+    }
+
+    return (
+        <Form {...form}>
+            <Toaster
+                position="bottom-center"
+                reverseOrder={false}
+            />
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full sm:w-2/3 space-y-6">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Name" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormItem>
+                    <FormLabel>email</FormLabel>
+
+                    <Input placeholder="email" value="smisdf@fsd.sdf" disabled />
+                    <FormMessage />
+                </FormItem>
+
+                <FormField
+                    control={form.control}
+                    name="phone"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>phone</FormLabel>
+                            <FormControl>
+                                <Input placeholder="+20 ..." {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Save</Button>
+            </form>
+        </Form>
+    )
+}
