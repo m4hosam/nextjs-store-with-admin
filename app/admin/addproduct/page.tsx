d260f4e8ffd3d3b3f210e3a624ca6e9c883737c2
MOHAMED HOSAM <mohamedhosaam154@gmail.com>
2023-09-01 09:39:46 +0200

    Add product normal form.

--- app/admin/addproduct/page.tsx
+++ app/admin/addproduct/page.tsx
@@ -8,11 +8,12 @@ import { ProductPropsAdmin } from '@/common.types';
 import { categoryFilters } from '@/constant';
 import CustomMenu from '@/components/ui/customMenu';
 import { createProduct } from '@/lib/actionAdmin'
-
+import toast, { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 export default function AddProduct() {
+    const router = useRouter();
     // State variable to track the visibility of the alert
-    const [isAlertVisible, setIsAlertVisible] = useState(false);
     const [form, setform] = useState<ProductPropsAdmin>({
         productName: '',
         category: '',
@@ -22,12 +23,6 @@ export default function AddProduct() {
         brand: '',
     })
 
-    const handleAlertClose = (e: React.MouseEvent<HTMLButtonElement>) => {
-        // Hide the alert
-        setIsAlertVisible(false);
-    }
-
-
     const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         // Send form object to backend
@@ -35,22 +30,16 @@ export default function AddProduct() {
             // Send form object to backend using fetch
             const response = await createProduct(form)
 
-            if (!response.ok) {
-                // Handle any error scenarios if needed
-                console.error('Form submission failed.');
-                return;
-            }
 
-            // Form submission successful, do something with the response if needed
-            const data = await response.json();
-            if (data.success) {
+            if (response.success) {
                 // toggle the alert to flex
-                setIsAlertVisible(true);
+                toast.success('Product successfully added!')
             }
             else {
                 // toggle the alert to hidden
-                setIsAlertVisible(false);
+                toast.error('An Error Occured')
             }
+            router.refresh();
 
         } catch (error) {
             // Handle any network or other errors
@@ -92,29 +81,11 @@ export default function AddProduct() {
             onSubmit={handleFormSubmit}
             className="w-full flex justify-around lg:px-32 md:px-14 px-5 py-7 gap-7 flex-wrap"
         >
-            {/* <button id="triggerElement" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Hide alert</button>
+            <Toaster
+                position="bottom-right"
+                reverseOrder={false}
+            />
 
-            <div id="targetElement" className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-blue-200 dark:text-blue-800" role="alert">
-                <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
-            </div> */}
-            <div id="alert-3"
-                className={`flex items-center p-4 mb-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 ${isAlertVisible ? 'flex' : 'hidden'
-                    }`}
-                role="alert">
-                <svg className="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-                </svg>
-                <span className="sr-only">Info</span>
-                <div className="ml-3 text-sm font-medium">
-                    Product has been added to the Database Successfully <a href="#" className="font-semibold underline hover:no-underline">See the product</a>.
-                </div>
-                <button onClick={handleAlertClose} type="button" className="ml-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-3" aria-label="Close">
-                    <span className="sr-only">Close</span>
-                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                    </svg>
-                </button>
-            </div>
             <div className="flexStart w-full lg:min-h-[200px] min-h-[200px] relative">
                 <label htmlFor="poster" className="flexCenter z-10 text-center w-full h-full p-20 text-gray-500 border-2 border-gray-200 border-dashed">
                     {!form.image && 'Choose a Product Image'}
